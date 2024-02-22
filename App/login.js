2f4d9610eb64c8e276cf442413f3dd8c54ea39b4
Abdullah Khan <abdullahmoves@gmail.com>
2024-02-21 23:36:51 -0500

    Added some Login Js code

--- App/login.js
+++ App/login.js
@@ -1,16 +1,42 @@
-
 const form = document.getElementById("UserInfo");
+const UserName = document.getElementById('UserName');
+const Password = document.getElementById('Password');
 
 
 
-async function GetJWT() {
-    const fromData = new FormData(form);
-    
+
+async function GetJWT(e) {
+    e.preventDefault();
+
+    const userName = UserName.value;
+    const password  = Password.value;
 
 
-    console.log(fromData.entries()['']);
+    const Token = "asfadsf"
+
+
+  
+
+    const repsonse = await fetch("http://localhost:3000/api/users/login",
+        {
+            method: "POST",
+            headers:{
+                "Authorization": `Bearer ${Token}`
+            },
+            body:{
+                name: userName
+            }
+            
+        }
+    )
+
+    console.log(repsonse.status);
+        
+
+
 }
 
+form.onsubmit = (e) =>  GetJWT(e)
 
 
-GetJWT();
+
