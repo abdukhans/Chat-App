1d2696b5f97cf8fe307eb643311f5e149bfc662e
Abdullah Khan <abdullahmoves@gmail.com>
2024-03-08 00:27:28 -0500

    added on save hot reload to typescript

---
+++ Server/server.ts
@@ -0,0 +1,26 @@
+
+import express, { Express, Request, Response } from "express";
+import *  as dotenv  from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+
+export function main(): Number{
+
+    var  s : Number = 31245;
+
+    return s;
+
+
+}
